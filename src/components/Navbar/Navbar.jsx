5b4f1c18e0120b6a7fe0e2db55a0b21c627fee28
Angela Montanez <angelamountains@gmail.com>
2024-06-04 18:06:38 +0200

    set up all component functions and css

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+function Navbar() {
+  return (
+    <header className="Header" id="Header">
+      <h1>Backmarket</h1>
+      <nav className="Navbar">
+        <ui>
+          <li>
+            <a href="#home"></a>
+          </li>
+          <li>
+            <a href="#inventory"></a>
+          </li>
+          <li>
+            <a href="#about"></a>
+          </li>
+        </ui>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
