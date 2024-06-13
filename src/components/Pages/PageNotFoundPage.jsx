18e39478158208acec47a7cca510d47321a7e6e9
Angela Montanez <angelamountains@gmail.com>
2024-06-13 10:06:34 +0200

    update font weight and move png to public folder

--- src/components/Pages/PageNotFoundPage.jsx
+++ src/components/Pages/PageNotFoundPage.jsx
@@ -1,11 +1,13 @@
 import "./PageNotFoundPage.css";
+import cryingSquirrel from "./../../assets/crying-squirrel@2x.png";
 
 const PageNotFound = () => {
   return (
     <div>
       <h1>404 - Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-      <img id="not-found-img" src="./src/assets/cry.png" alt="" />
+      {/* <img id="not-found-img" src="/crying-squirrel@2x.png" alt="" /> */}
+      <img id="not-found-img" src={cryingSquirrel} alt="" />
       <p>
         <a href="/">Go to Home</a>
       </p>
